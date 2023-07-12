58852f89b46e61813bf206dcf276e797909c3407
estarhp <3136551640@qq.com>
2023-07-12 12:14:37 +0800

    feat : Switch 基本完成

--- src/Switch/index.md
+++ src/Switch/index.md
@@ -6,7 +6,17 @@ nav:
 
 ```tsx
 import {Switch} from "E-UI";
+import {useState} from "react";
 
-export default () => <Switch></Switch>
+
+function handleClick(event) {
+  console.log(event.target.checked)
+}
+
+export default () => <Switch 
+  prefix={<div>选择左</div>}
+  suffix={<div>选择右</div>}
+  handleClick={handleClick}
+></Switch>
 
 ```
--- src/Switch/index.tsx
+++ src/Switch/index.tsx
@@ -1,23 +1,34 @@
-import React,{forwardRef} from "react";
+import React, {forwardRef, MouseEventHandler} from "react";
 import classNames from "classnames";
 import "./style/index.scss"
 
 export interface SwitchProps {
+   prefix?:React.ReactNode
+   suffix?:React.ReactNode
+  disabled?:boolean
+  handleClick:React.MouseEventHandler<HTMLInputElement>
+  handleChange: React.ChangeEventHandler<HTMLInputElement>
 
 }
 
 const Switch=forwardRef<HTMLDivElement,SwitchProps>((props,ref) =>{
-  const {} = props;
+  const {
+    prefix,
+    suffix,
+    handleClick,
+    disabled=false,
+    handleChange
+  } = props;
   const classes  = classNames("e-switch",{
-
+    "e-switch-disabled":disabled
   })
+  return <div className={classes} >
 
-  function handleClick (event: { target: any; }){
+      <input   type="checkbox" id="toggle" style={{display:"none"}} onClick={handleClick} onChange={handleChange} disabled={disabled}/>
+      {prefix&& <label htmlFor="toggle" className="e-switch-prefix">{prefix}</label>}
+      <label htmlFor="toggle" className="e-switch-label"></label>
+      {suffix && <label htmlFor="toggle" className="e-switch-suffix">{suffix}</label>}
 
-  }
-  return <div className={classes}>
-    <input  onClick={handleClick} type="checkbox" id="toggle" style={{display:"none"}}/>
-    <label htmlFor="toggle" className="e-switch-label"></label>
   </div>
 })
 
